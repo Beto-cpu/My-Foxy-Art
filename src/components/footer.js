a174e9a4c0c502ad393247b116ef4365d568bcb4
Beto-cpu <beto.munguia.lara@gmail.com>
2022-10-01 21:44:40 -0700

    Image generator page

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import { BiMenuAltRight } from 'react-icons/bi';
 
 export default function Footer(){
     return (
-        <div className="p-5 text-center">
+        <div className="p-5 text-center mt-3">
             DEVELOPED BY FOX CODING FOR SPACE APPS 2022
         </div>
     )
